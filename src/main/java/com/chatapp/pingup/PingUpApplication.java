7e8993f463dc699050597b43f5ffd894637005e4
NaBa26 <namankumar777@gmail.com>
2024-11-07 18:02:56 +0530

    Linking remote repo with the local directory

---
+++ src/main/java/com/chatapp/pingup/PingUpApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.pingup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatapp/pingup/PingUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.pingup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
